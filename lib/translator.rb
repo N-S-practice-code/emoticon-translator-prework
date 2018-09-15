0e172b956f3cf41bcdfb8ca2a31456d0c5d34ca7
Nico-ehs <nico.ehs05@gmail.com>
2018-09-15 21:22:06 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,8 +5,12 @@ require 'yaml'
 
 def load_library(file)
   h1=YAML.load_file(file)
+  puts h1
   r={"get_emoticon" => {}, "get_meaning" => {}}
   h1.each do |k1, v1|
+    
+  end
+  r1
 end
 
 def get_japanese_emoticon
